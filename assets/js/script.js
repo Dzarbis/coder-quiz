3488efa6e37a8994ace107dd9b5c810e1b254cc2
Dzarbis <dzarbis@gmail.com>
2021-03-20 20:37:38 -0400

    questions added

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "A boolean is ____?",
+        options: ["a true/false statement", "an array containing another array", "a listing of variables", "a particularly lazy ghost"],
+        answer: "a true/false statement"
+    },
+    {
+        question: "A great way to check if your code is working is ____.",
+        options: ["console.log()", "debugger", "testing it yourself in the browser", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "Which of the following is NOT a JavaScript data type?",
+        options: ["Object", "String", "Null", "Console"],
+        answer: "Console"
+    },
+    {
+        question: "An 'else if' statement must be preceded by _____",
+        options: ["an 'else' statement", "a variable", "an 'if' statement", "a for loop"],
+        answer: "an 'if' statement"
+    },
+    {
+        question: "Which is the proper way to show 'or' in a condition?",
+        options: ["or", "||", "!==", "&&"],
+        answer: "||"
+    }
+];
